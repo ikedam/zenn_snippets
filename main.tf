e0fe260fcd206b00ec59d369a4eabd4cc8fc532b
ikedam <devld@ikedam.jp>
2024-04-30 15:48:39 +0900

    allow deleting ECR

--- main.tf
+++ main.tf
@@ -38,6 +38,7 @@ provider "docker" {
 resource "aws_ecr_repository" "image_repository" {
   name                 = replace(var.basename, "-", "_")
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
