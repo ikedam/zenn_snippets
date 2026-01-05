872d4d02a52e6f93561178b988bf7d2097404800
ikedam <devld@ikedam.jp>
2026-01-05 15:52:06 +0900

    検証パターンを追加

--- variables.tf
+++ variables.tf
@@ -3,8 +3,16 @@ variable "google_project" {
   description = "Google Cloud プロジェクト名"
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "basename" {
   type        = string
   description = "構築するリソースの共通prefix"
   default     = "ephemeraltest"
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "source_secret_id" {
+  type        = string
+  description = "手動作成したコピー元のシークレットID"
+  default     = "ephemeraltest-copy-source"
 }
